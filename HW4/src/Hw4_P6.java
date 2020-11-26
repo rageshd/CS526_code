f3bd1d411a0858abd78f7ebabbf070dc92620071
beowulf10 <>
2020-11-26 09:21:41 -0800

    minor changes

--- HW4/src/Hw4_P6.java
+++ HW4/src/Hw4_P6.java
@@ -11,21 +11,91 @@ public class Hw4_P6 {
         // Declare the variables
         HashMap myMap = new HashMap();
         ArrayList myArrayList = new ArrayList();
-        LinkedList LinkedList = new LinkedList();
+        LinkedList myLinkedList = new LinkedList();
         int[] insertKeys = new int[ARRAY_SIZE];
         int[] searchKeys = new int[ARRAY_SIZE];
+        long startTime, endTime, elapsedTime;
+        long totalMapInsertTime = 0, totalArrayListInsertTime=0, totalLinkedListInsertTime=0;
+        long totalMapSearchTime=0, totalArrayListSearchTime=0, totalLinkedListSearchTime=0;
 
-        // Generate random integers and populate the insertKeys array
+        // Generate random integers
         Random r = new Random(System.currentTimeMillis() );
-        for( int i = 0; i< ARRAY_SIZE; i++)
-            insertKeys[i] = r.nextInt(ARRAY_SIZE) + 1;
 
-        // Reseed the random
-        r.setSeed(System.currentTimeMillis());
+        // Run the operations 10 times
+        for (int j = 0; j < 10; j++) {
 
-        // Generate random integers and populate the searchKeys array
-        for( int i = 0; i< ARRAY_SIZE; i++)
-            searchKeys[i] = r.nextInt(ARRAY_SIZE) + 1;
+            // Reseed the random
+            r.setSeed(System.currentTimeMillis());
+            // Populate the insertKeys
+            for (int i = 0; i < ARRAY_SIZE; i++)
+                insertKeys[i] = r.nextInt(ARRAY_SIZE) + 1;
+
+            // Insert into map
+            startTime = System.currentTimeMillis();
+            for (int i : insertKeys)
+                myMap.put(i,i);
+            endTime = System.currentTimeMillis();
+            elapsedTime = endTime - startTime;
+            totalMapInsertTime += elapsedTime;
+
+            // Add to Arraylist
+            startTime = System.currentTimeMillis();
+            for (int i : insertKeys)
+                myArrayList.add(i);
+            endTime = System.currentTimeMillis();
+            elapsedTime = endTime - startTime;
+            totalArrayListInsertTime += elapsedTime;
+
+            // Add to LinkedList
+            startTime = System.currentTimeMillis();
+            for (int i : insertKeys)
+                myLinkedList.add(i);
+            endTime = System.currentTimeMillis();
+            elapsedTime = endTime - startTime;
+            totalLinkedListInsertTime += elapsedTime;
+
+            // Reseed the random
+            r.setSeed(System.currentTimeMillis());
+
+            // Generate random integers and populate the searchKeys array
+            for (int i = 0; i < ARRAY_SIZE; i++)
+                searchKeys[i] = r.nextInt(2*ARRAY_SIZE) + 1;
+
+            // Search in map
+            startTime = System.currentTimeMillis();
+            for (int i : searchKeys)
+                myMap.get(i);
+            endTime = System.currentTimeMillis();
+            elapsedTime = endTime - startTime;
+            totalMapSearchTime += elapsedTime;
+
+            // Search in arraylist
+            startTime = System.currentTimeMillis();
+            for (int i : searchKeys)
+                myArrayList.contains(i);
+            endTime = System.currentTimeMillis();
+            elapsedTime = endTime - startTime;
+            totalArrayListSearchTime += elapsedTime;
+
+            // Search in linkedlist
+            startTime = System.currentTimeMillis();
+            for (int i : searchKeys)
+                myLinkedList.contains(i);
+            endTime = System.currentTimeMillis();
+            elapsedTime = endTime - startTime;
+            totalLinkedListSearchTime += elapsedTime;
+
+        }
+
+        // Print the output
+        System.out.printf("Number of keys = %d\n\n", ARRAY_SIZE);
+        System.out.printf("HashMap average total insert time = %.2f\n", totalMapInsertTime/10);
+        System.out.printf("ArrayList average total insert time = %.2f\n", totalArrayListInsertTime/10);
+        System.out.printf("LinkedList average total insert time = %.2f\n\n", totalLinkedListInsertTime/10);
+
+        System.out.printf("HashMap average total search time = %.2f\n", totalMapSearchTime/10);
+        System.out.printf("ArrayList average total search time = %.2f\n", totalArrayListSearchTime/10);
+        System.out.printf("LinkedList average total search time = %.2f\n\n", totalLinkedListSearchTime/10);
 
 
     }
